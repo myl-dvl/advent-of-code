1b0c84ec9d28d1cb93b54318e1e0d568a3a22d27
m.duval <mduval@beapi.tech>
2025-12-03 13:57:51 +0100

    feat: compute puzzle input

--- day2/day2.py
+++ day2/day2.py
@@ -1,4 +1,7 @@
+from pathlib import Path
 from typing import List, Tuple
+
+from utils import import_text
 
 
 def check_if_repetition(id_to_test: int) -> bool:
@@ -26,9 +29,16 @@ def extract_sides(input_range: str) -> Tuple[int, int]:
     start, end = input_range.split('-')
     return int(start), int(end)
 
+
 def compute_input(input_str: str) -> int:
     count = 0
     for range_str in input_str.split(','):
         list_invalid = search_invalid(range_str)
         count += sum(list_invalid)
     return count
+
+
+if __name__ == '__main__':
+    input_str = import_text(Path('day2.txt'))
+    print(f'Sum replication ids: {compute_input(input_str[0])}')
+    # 22062284697
